71b9eee56fa47ae177249bf02f12d7c965b3cbb0
idealaswani <aswani@henryharvin.in>
2025-07-31 17:59:51 +0530

    change adress

--- resources/views/accreditation.blade.php
+++ resources/views/accreditation.blade.php
@@ -9,53 +9,65 @@
             <!-- Card 1 -->
             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
                 <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1724768847Screenshot_2024-08-27_190332.png" alt="AAEFL" class="h-28 w-auto">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1724768847Screenshot_2024-08-27_190332.png" loading="lazy " alt="AAEFL" class="h-28 w-auto">
                 </div>
-
-            </div>
-
-            <!-- Card 2 -->
-            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
-                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753255971sxsssswc.PNG" alt="AAEFL" class=" h-28 w-auto">
-                </div>
-
-            </div>
-
-            <!-- Card 3 -->
-            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
-                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1726071455watts_we_logo_jft.png" alt="AAEFL" class=" h-28 w-auto">
-                </div>
-
-            </div>
-
-            <!-- Card 4 -->
-            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
-                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1736849226DBS_C-01.png" alt="AAEFL" class=" h-28 w-auto">
-                </div>
-
             </div>
             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
                 <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://flism.org/wp-content/uploads/2025/01/Logo_Florida_7.jpg" alt="AAEFL" class=" h-16 w-auto">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753255971sxsssswc.PNG" loading="lazy " alt="AAEFL56" class=" h-28 w-auto">
+                </div>
+            </div>
+            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1726071455watts_we_logo_jft.png" loading="lazy " alt="AAEF565L" class=" h-28 w-auto">
+                </div>
+            </div>
+            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1736849226DBS_C-01.png" loading="lazy " alt="AAEFLewe" class=" h-28 w-auto">
+                </div>
+            </div>
+            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753943295ezgif.com-resize_(2).webp" loading="lazy " alt="AAEFdfhL" class=" h-16 w-auto">
                 </div>
             </div>
             <!-- Card 5 -->
             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
                 <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753256081hhlogo.png" alt="AAEFL" class=" h-16 w-auto">
+                    <img src="https://cdn.henryharvin.com/images/1686549627AAEFL.webp" loading="lazy " alt="AAEFL897" class=" h-24 w-auto">
+                </div>
+            </div>
+            <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex gap-2 justify-center">
+                    <img src="https://nsdcindia.org/logo_nsdc.svg" loading="lazy " alt="AAEFLfsdf" class=" h-16 w-auto">
+                        <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753882471skills.svg" loading="lazy " alt="AAEFLsde" class=" h-16 w-auto">
+
                 </div>
             </div>
             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
                 <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://kodakco.sgp1.cdn.digitaloceanspaces.com/1702461434kodacko.webp" alt="AAEFL" class=" h-16 w-auto">
+                    <img src="https://cdn.henryharvin.com/images/1683959258ezgif.com-gif-maker%20-%202023-05-13T115731.582.webp" loading="lazy " alt="AAEFL" class=" h-24 w-auto">
                 </div>
             </div>
             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
                 <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
-                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/171514477432e3rr.webp" alt="AAEFL" class=" h-24 w-auto">
+                    <img src="https://cdn.henryharvin.com/images/1683959347ezgif.com-resize%20(20).webp" loading="lazy " alt="AAEFLcxcvcxz" class=" h-24 w-auto">
+                </div>
+            </div>
+             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753941817ezgif.com-resize_(1).webp" loading="lazy " alt="AAEFLqwdD" class=" h-24 w-auto">
+                </div>
+            </div>
+             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://cdn.henryharvin.com/images/1683958602ezgif.com-resize%20(16).webp" loading="lazy " alt="AAEFLFGEFF" class=" h-24 w-auto">
+                </div>
+            </div>
+             <div class="bg-white p-4 grid place-content-evenly place-items-center gap-4">
+                <div class="w-full mx-auto text-center overflow-hidden flex justify-center">
+                    <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753941817ezgif.com-resize_(1).webp" loading="lazy " alt="AAEFDFEEGEL" class=" h-24 w-auto">
                 </div>
             </div>
         </div>
