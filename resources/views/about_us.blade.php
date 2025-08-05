6fdd77cbf780eca0e5bfc8f0266fbc98bf5de45c
idealaswani <aswani@henryharvin.in>
2025-08-05 11:02:03 +0530

    change adress

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -203,8 +203,6 @@
                     <p class="text-white text-[14px] text-center">Advisory Board Member</p>
                 </div>
             </div>
-
-            <!-- Mrs. Ishu Jain -->
             <div class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
                 <div class="rounded-2xl">
                     <img class="h-full w-full object-cover rounded-2xl"
@@ -216,8 +214,6 @@
                     <p class="text-white text-[14px] text-center">Advisory Board Member</p>
                 </div>
             </div>
-
-            <!-- Mrs. Seema Rampal -->
             <div class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
                 <div class="rounded-2xl">
                     <img class="h-full w-full object-cover rounded-2xl"
@@ -229,8 +225,6 @@
                     <p class="text-white text-[14px] text-center">Advisory Board Member</p>
                 </div>
             </div>
-
-            <!-- Mrs. Seema Rampal -->
             <div class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
                 <div class="rounded-2xl">
                     <img class="h-full w-full object-cover rounded-2xl"
@@ -239,7 +233,7 @@
                 <div class="px-2">
                     <h2 class="text-white text-center text-[16px] pb-2">Toshiyuki Takayasu</h2>
                     {{-- <p class="text-white text-[14px] text-center font-[500]">Head of Faculty</p> --}}
-                    <p class="text-white text-[14px] text-center">Advisory Board Member</p>
+                    <p class="text-white text-[14px] text-center">Board Advisor</p>
                 </div>
             </div>
             <div class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
@@ -250,7 +244,7 @@
                 <div class="px-2">
                     <h2 class="text-white text-center text-[16px] pb-2">Chinami Yamamoto</h2>
                     {{-- <p class="text-white text-[14px] text-center font-[500]">Head of Faculty</p> --}}
-                    <p class="text-white text-[14px] text-center">Advisory Board Member</p>
+                    <p class="text-white text-[14px] text-center">Board Advisor</p>
                 </div>
             </div>
         </div>
