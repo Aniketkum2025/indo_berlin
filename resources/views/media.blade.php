bea3f45eac169f2f56261aa841bd1c028a2a71f4
idealaswani <aswani@henryharvin.in>
2025-08-14 12:32:30 +0530

    update course page in kazumi

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -1,4 +1,74 @@
 @extends('layouts.app')
+@section('title', 'Media Kazumi Japanese Language School | Powered by Henry Harvin')
+@section('meta_description', 'Kazumi Japanese & Culture School by Henry Harvin® helps students master the Japanese
+    language and culture through immersive instruction by experts.')
 @section('content')
+
+    <section class="py-12 bg-gray-100">
+        <div class="max-w-7xl mx-auto px-4">
+            <h2 class="text-center text-2xl md:text-3xl font-semibold mb-10 text-[#da2028] capitalize my-4">Media Coverage
+            </h2>
+            <div class="grid space-y-4">
+
+                <div class="max-w-3xl mx-auto bg-white border rounded-lg shadow-md p-6 flex gap-4">
+                    <div class="grid gap-6">
+                        <!-- Heading -->
+                        <div class="grid md:flex gap-4 items-center">
+                            <div class="flex-shrink-0 border border-lg shadow-lg p-2 h-full flex justifu-center items-center">
+                                <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1755153683iedLogo-1000x.webp" alt="Logo" class="h-10 object-contain">
+                            </div>
+
+                            <h2 class="text-[20px] md:text-[24px] font-[700] text-red-600">
+                                Henry Harvin Launches Kazumi: A Premier Japanese Language & Culture School for Global Learners
+                            </h2>
+                        </div>
+
+                        <!-- Description -->
+                        <p class="mt-2 text-gray-700 line-clamp-8">
+                           “Kazumi is not just a language school; it’s a bridge to a new world,” said Kounal Gupta Founder & CEO Henry Harvin. “With Japan emerging as a top destination for skilled professionals and cultural exchange, we saw a clear need for a dedicated, end-to-end training solution. Kazumi fills that gap — guiding learners from Hiragana to Keigo, and from grammar drills to etiquette and immersion.”
+                        </p>
+
+                        <!-- Read More Button -->
+                        <div class="flex justify-between items-center">
+                        <a href="https://indiaeducationdiary.in/henry-harvin-launches-kazumi-a-premier-japanese-language-culture-school-for-global-learners/"
+                            class="inline-block mt-3 px-4 py-2 bg-red-600 text-white rounded-md shadow hover:bg-red-700 transition">
+                            Read More
+                        </a>
+                        <p class="text-red-600">Thursday, August 14, 2025</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="max-w-3xl mx-auto bg-white border rounded-lg shadow-md p-6 flex gap-4">
+                    <div class="grid gap-6">
+                        <!-- Heading -->
+                        <div class="grid md:flex gap-4 items-center">
+                            <div class="flex-shrink-0 border border-lg shadow-lg p-2 h-full flex justifu-center items-center">
+                                <img src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1755154721businessnewsthisweek-logo-1.png" alt="Logo" class="h-10 object-contain">
+                            </div>
+
+                            <h2 class="text-[20px] md:text-[24px] font-[700] text-red-600">
+                               Henry Harvin Launches Kazumi: A Premier Japanese Language and Culture School for Global Learners
+                            </h2>
+                        </div>
+
+                        <!-- Description -->
+                        <p class="mt-2 text-gray-700 line-clamp-8">
+                           “Kazumi is not just a language school; it’s a bridge to a new world,” said Kounal Gupta Founder & CEO Henry Harvin. “With Japan emerging as a top destination for skilled professionals and cultural exchange, we saw a clear need for a dedicated, end-to-end training solution. Kazumi fills that gap — guiding learners from Hiragana to Keigo, and from grammar drills to etiquette and immersion.”
+                        </p>
+
+                        <!-- Read More Button -->
+                        <div class="flex justify-between items-center">
+                        <a href="https://indiaeducationdiary.in/henry-harvin-launches-kazumi-a-premier-japanese-language-culture-school-for-global-learners/"
+                            class="inline-block mt-3 px-4 py-2 bg-red-600 text-white rounded-md shadow hover:bg-red-700 transition">
+                            Read More
+                        </a>
+                        <p class="text-red-600">Thursday, August 14, 2025</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 @endsection
