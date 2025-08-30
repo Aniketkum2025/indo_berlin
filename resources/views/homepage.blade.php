f1c36d114eb9d84a2dcde2468b61909e8d967b1e
idealaswani <aswani@henryharvin.in>
2025-08-30 10:22:46 +0530

    changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -812,46 +812,7 @@
             <h2 class="text-center text-2xl md:text-3xl font-semibold mb-10 text-[#da2028] capitalize my-4">Kazumi Advisory
                 Board Members</h2>
             <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-2 place-items-center px-2">
-                <!-- Mr. Kounal Gupta -->
-                <div
-                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
-                    <div class="rounded-2xl">
-                        <img class="h-full w-full object-cover rounded-2xl"
-                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961762KounalGupta.jpg"
-                            alt="Kounal Gupta – CEO & Founder Kazumi Japanese & Culture School">
-                    </div>
-                    <div class="px-2">
-                        <h2 class="text-white text-center text-[16px] pb-2">Mr. Kounal Gupta</h2>
-                        <p class="text-white text-[14px] text-center font-[500]">CEO, Henry Harvin Education</p>
-                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
-                    </div>
-                </div>
-                <div
-                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
-                    <div class="rounded-2xl">
-                        <img class="h-full w-full object-cover rounded-2xl"
-                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961907IshuJain.jpg"
-                            alt=" Ishu Jain – Head of Academics">
-                    </div>
-                    <div class="px-2">
-                        <h2 class="text-white text-center text-[16px] pb-2">Mrs. Ishu Jain</h2>
-                        <p class="text-white text-[14px] text-center font-[500]">Head of Academics</p>
-                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
-                    </div>
-                </div>
-                <div
-                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
-                    <div class="rounded-2xl">
-                        <img class="h-full w-full object-cover rounded-2xl"
-                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961856seemarampal.jpg"
-                            alt="Seema Rampal – Head of Faculty">
-                    </div>
-                    <div class="px-2">
-                        <h2 class="text-white text-center text-[16px] pb-2">Mrs. Seema Rampal</h2>
-                        <p class="text-white text-[14px] text-center font-[500]">Head of Faculty</p>
-                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
-                    </div>
-                </div>
+               
                 <div
                     class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
                     <div class="rounded-2xl">
@@ -889,6 +850,46 @@
                         <h2 class="text-white text-center text-[16px] pb-2">Daisuke Yamanaka</h2>
                         {{-- <p class="text-white text-[14px] text-center font-[500]">Head of Faculty</p> --}}
                         <p class="text-white text-[14px] text-center">Board Advisor</p>
+                    </div>
+                </div>
+                 <!-- Mr. Kounal Gupta -->
+                <div
+                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
+                    <div class="rounded-2xl">
+                        <img class="h-full w-full object-cover rounded-2xl"
+                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961762KounalGupta.jpg"
+                            alt="Kounal Gupta – CEO & Founder Kazumi Japanese & Culture School">
+                    </div>
+                    <div class="px-2">
+                        <h2 class="text-white text-center text-[16px] pb-2">Mr. Kounal Gupta</h2>
+                        <p class="text-white text-[14px] text-center font-[500]">CEO, Henry Harvin Education</p>
+                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
+                    </div>
+                </div>
+                <div
+                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
+                    <div class="rounded-2xl">
+                        <img class="h-full w-full object-cover rounded-2xl"
+                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961907IshuJain.jpg"
+                            alt=" Ishu Jain – Head of Academics">
+                    </div>
+                    <div class="px-2">
+                        <h2 class="text-white text-center text-[16px] pb-2">Mrs. Ishu Jain</h2>
+                        <p class="text-white text-[14px] text-center font-[500]">Head of Academics</p>
+                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
+                    </div>
+                </div>
+                <div
+                    class="group bg-black border border-red-500 rounded-2xl shadow-lg p-4 w-full max-w-xs transition duration-300 hover:bg-red-600 hover:shadow-xl">
+                    <div class="rounded-2xl">
+                        <img class="h-full w-full object-cover rounded-2xl"
+                            src="https://d1d5cy0fmpy9m8.cloudfront.net/images/1753961856seemarampal.jpg"
+                            alt="Seema Rampal – Head of Faculty">
+                    </div>
+                    <div class="px-2">
+                        <h2 class="text-white text-center text-[16px] pb-2">Mrs. Seema Rampal</h2>
+                        <p class="text-white text-[14px] text-center font-[500]">Head of Faculty</p>
+                        {{-- <p class="text-white text-[14px] text-center">Board Advisor</p> --}}
                     </div>
                 </div>
             </div>
