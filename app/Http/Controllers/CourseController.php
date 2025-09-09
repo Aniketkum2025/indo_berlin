0a2aa15a182d2ed29c190c16bb2d20c52badc5c0
fakarealam <fakare.alam@henryharvin.in>
2025-09-09 12:00:53 +0530

    CourseController.php

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -22,13 +22,17 @@ class CourseController extends Controller
 {
     public function index(Request $request, $slug) {
 
-        if(!str_contains($slug, 'japan')){
-            return abort(404);
-        }
+        // if(!str_contains($slug, 'japan')){
+        //     return abort(404);
+        // }
 
          $course = Cache::remember('course_slug_' . $slug, 60 * 60, function () use ($slug) {
             return Course::where('slug', '=', $slug)->first();
         });
+
+        if(!$course || $course->website != 'kazumi'){
+            return abort(404);
+        }
         
         if ($course) {
             $result = Cache::remember('result_slug_' . $course->crm_course_id, 60 * 60, function () use ($course) {
